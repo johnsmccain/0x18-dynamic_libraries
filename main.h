b6412909e75141de96dd088916738f2ebbea284e
John Danlami <35366478+johnsmccain@users.noreply.github.com>
2022-06-13 19:03:51 +0100

    Update main.h

--- main.h
+++ main.h
@@ -1,16 +1,16 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef DYNLIB
+#define DYNLIB
 
 #include <stdlib.h>
 
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
-int _abs(int c);
+int _abs(int n);
 int _isupper(int c);
 int _isdigit(int c);
 int _strlen(char *s);
-void _put(char *s);
+void _puts(char *s);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 char *_strcat(char *dest, char *src);
